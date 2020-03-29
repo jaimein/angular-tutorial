fcfef78a0ca7b641eb9b13abfaf47f03b00f07f2
jaimein <jaimeiserte@hotmail.com>
2020-03-29 12:01:18 +0200

    1 file modified

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+
+import { products } from "../products";
+import { ActivatedRoute } from "@angular/router";
 
 @Component({
-  selector: 'app-product-details',
-  templateUrl: './product-details.component.html',
-  styleUrls: ['./product-details.component.css']
+  selector: "app-product-details",
+  templateUrl: "./product-details.component.html",
+  styleUrls: ["./product-details.component.css"]
 })
 export class ProductDetailsComponent implements OnInit {
+  product;
+  constructor(private route: ActivatedRoute) {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
